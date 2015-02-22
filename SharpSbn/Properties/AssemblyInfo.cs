b6d15eabf72b79040d2b446c9d4640b7e78802b9
FObermaier <felix.obermaier@netcologne.de>
2015-02-22 15:23:44 +0100

    Update dependency on GeoAPI Update nuspec files Update AssemblyVersion

--- SharpSbn/Properties/AssemblyInfo.cs
+++ SharpSbn/Properties/AssemblyInfo.cs
@@ -35,7 +35,7 @@ using System.Runtime.InteropServices;
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
 [assembly: InternalsVisibleTo("SharpSbn.Test")]
 #if !PCL
 [assembly: CLSCompliant(true)]
